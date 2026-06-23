0e25aab371658ba629808f4f607855930961434c
Adam Liewehr <al8425b@american.edu>
2026-06-23 16:59:20 -0400

    bubbles!

--- gettingStarted/bubbleExample/bubble.js
+++ gettingStarted/bubbleExample/bubble.js
@@ -4,18 +4,45 @@ class Bubble {
     this.y = y;
 
     // eventually make it so that if you hold down for longer the bubble get bigger!
-    this.r = 50;
-    this.dx = 0;
-    this.dy = -10;
+    this.d = 50;
+    this.r = this.d / 2;
+    this.dx = random(-5, 5);
+    this.dy = random(-5, 5);
   }
 
   move() {
     this.x += this.dx;
     this.y += this.dy;
+    this.r = this.d / 2; // update the radius
   }
 
   display() {
     stroke(0);
-    circle(this.x, this.y, this.r);
+    circle(this.x, this.y, this.d);
+  }
+
+  checkColision() {
+    // console.log(width);
+    // console.log(this.y + this.r);
+    if (this.y - this.r <= 0) {
+      // circle collides with top of canvas
+      this.dy *= -1;
+    }
+    if (this.y + this.r >= height) {
+      this.dy *= -1;
+    }
+    if (this.x - this.r < 0) {
+      this.dx *= -1;
+    }
+    if (this.x + this.r > width) {
+      this.dx *= -1;
+    }
+  }
+
+  checkForBugged() {
+    if (this.x > width || this.x < 0 || this.y > height || this.y < 0) {
+      this.x = width / 2;
+      this.y = height / 2;
+    }
   }
 }
--- gettingStarted/bubbleExample/sketch.js
+++ gettingStarted/bubbleExample/sketch.js
@@ -1,19 +1,53 @@
+// defines an array to store all the bubblese in
 let bubbles = [];
+// this is needed for the click and drag effect when spawning the bubbles
+// we need this to check if its the first step that the use has pressed down the mouse
+let firstStep = true;
 
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(windowWidth, windowHeight);
+
   background(200);
 }
+
+// setting the initial bubble global so it can be acsessed anywhere
+let bub;
 
 function draw() {
   background(200);
+
+  // main bubble drawing a movement loop
   for (let bubble of bubbles) {
     bubble.display();
     bubble.move();
+    bubble.checkColision();
+    bubble.checkForBugged();
+  }
+
+  if (mouseIsPressed) {
+    // console.log(mouseX, mouseY);
+    if (firstStep) {
+      // if its the first step in this mouse press that the mouse is being held
+      bub = new Bubble(mouseX, mouseY); // create a bubble
+      bub.display();
+    } else {
+      // the bubble moves with the mouse and grows until the mouse is released
+      bub.x = mouseX;
+      bub.y = mouseY;
+      bub.d += 1;
+      bub.display();
+    }
+
+    firstStep = false;
   }
 }
 
-function mousePressed() {
-  bubbles.push(new Bubble(mouseX, mouseY));
-  // console.log(mouseX, mouseY);
+function mouseReleased() {
+  firstStep = true;
+  bubbles.push(bub); // push the released bubble to the array
+}
+
+// Automatically triggers whenever the browser window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
